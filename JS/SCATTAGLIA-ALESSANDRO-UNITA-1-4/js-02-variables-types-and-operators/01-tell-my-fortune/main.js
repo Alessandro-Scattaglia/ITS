d1d3dde7d690af7561cdc5596bf734cdd356d6c8
Alessandro-Scattaglia <79847731+Alessandro-Scattaglia@users.noreply.github.com>
2025-02-18 12:20:25 +0100

    add es_unit2_js

---
+++ JS/SCATTAGLIA-ALESSANDRO-UNITA-1-4/js-02-variables-types-and-operators/01-tell-my-fortune/main.js
@@ -0,0 +1,6 @@
+let x = "Cameriera";
+let y = "Italia";
+let z = "monica"
+let n = 2
+
+console.log(`You will be a ${x} in ${y} and married to ${z} with ${n} kids.`);
---
+++ JS/SCATTAGLIA-ALESSANDRO-UNITA-1-4/js-02-variables-types-and-operators/03-free-coffee/main.js
@@ -0,0 +1,10 @@
+let eta = 19;
+let max_eta = 99;
+
+let caffe_giornalieri = 2;
+
+let count = (2*365)*(max_eta-eta);
+
+
+console.log(`You will need ${count} cups of coffee to last you until the
+ripe old age of ${max_eta}`)
