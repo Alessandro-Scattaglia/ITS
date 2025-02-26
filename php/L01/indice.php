ee0dbbb15f656ca77161ef414ee5b225b26d172e
Alessandro-Scattaglia <79847731+Alessandro-Scattaglia@users.noreply.github.com>
2025-02-26 12:58:57 +0100

    add php

---
+++ php/L01/indice.php
@@ -0,0 +1,9 @@
+<?php define('TITOLO', 'Benvenuto nel mio sito web PHP powered'):?>
+<?php include 'intestazione.php';?>
+
+<h1><?= TITOLO ?></h1>
+
+<p>Oggi è il <?= date('d/m/Y') ?></p>
+<p>Sono le ore <?= date('h:i:s') ?></p>
+
+<?php $materia = "php"; ?>
