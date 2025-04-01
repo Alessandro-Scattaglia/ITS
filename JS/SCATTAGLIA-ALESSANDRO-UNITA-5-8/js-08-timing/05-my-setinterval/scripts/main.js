061dcf712d2360e7e74d5f5364071a93454d98f6
Alessandro-Scattaglia <79847731+Alessandro-Scattaglia@users.noreply.github.com>
2025-04-01 17:57:14 +0200

    correct

--- JS/SCATTAGLIA-ALESSANDRO-UNITA-5-8/js-08-timing/05-my-setinterval/scripts/main.js
+++ JS/SCATTAGLIA-ALESSANDRO-UNITA-5-8/js-08-timing/05-my-setinterval/scripts/main.js
@@ -14,16 +14,12 @@
  * // "Counter: 15"
  */
 function mySetInterval(callback, delay) {
-    let counter = 1;
-    function repeat() {
-        callback();
-        console.log(`Counter: ${counter}`);
-        if (counter < 15) {
-            setTimeout(repeat, delay);
-        }
-        counter++;
+    for (let i = 1; i <= 15; i++) {
+        setTimeout(() => {
+            callback();
+            console.log(`Counter: ${i}`);
+        }, delay * i);
     }
-    setTimeout(repeat, delay);
 }
 
 mySetInterval(() => console.log("Hello!"), 1000);
--- JS/SCATTAGLIA-ALESSANDRO-UNITA-5-8/js-08-timing/08-timed-conversion/scripts/main.js
+++ JS/SCATTAGLIA-ALESSANDRO-UNITA-5-8/js-08-timing/08-timed-conversion/scripts/main.js
@@ -4,21 +4,39 @@
  * Converts temperatures between Celsius and Fahrenheit every second using `setInterval`.
  * @example
  * // Outputs (repeating every second):
- * // "0°C is 32°F"
- * // "32°F is 0°C"
+ * // "0°C converted to Fahrenheit is 32°F"
+ * // "1°C converted to Fahrenheit is 33.8°F"
+ * // "2°C converted to Fahrenheit is 35.6°F"
+ * etc.
  */
 function celsiusToFahrenheit(celsius) {
-    console.log(`${celsius}°C is ${(celsius * 9 / 5) + 32}°F`);
+    console.log(`${celsius}°C converted to Fahrenheit is ${(celsius * 9 / 5) + 32}°F`);
 }
 
-function fahrenheitToCelsius(fahrenheit) {
-    console.log(`${fahrenheit}°F is ${(fahrenheit - 32) * 5 / 9}°C`);
+
+//1st method: setInterval
+let counter = 0;
+setInterval(() => {
+    if (counter <= 100) {
+        celsiusToFahrenheit(counter);
+    }
+    counter++;
+}, 1000);
+
+
+
+
+
+
+//2nd method: setTimeout (recursive)
+let counter1 = 0;
+function loop() {
+    if (counter1 <= 100) {
+        celsiusToFahrenheit(counter1);
+        counter1++;
+        setTimeout(loop, 1000);
+    }
 }
+loop();
 
-let timerId = setInterval(() => {
-    celsiusToFahrenheit(0);
-}, 1000);
 
-let timerId1 = setInterval(() => {
-    fahrenheitToCelsius(32);
-}, 1000);
